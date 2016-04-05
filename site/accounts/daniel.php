bed1ad0b37fbecc611caeda28691064cde7cb86e
Daniel Swakman <dswakman@oefenweb.nl>
2016-04-05 21:32:31 +0300

    kirby v2.2.3

---
+++ site/accounts/daniel.php
@@ -0,0 +1,8 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: daniel
+email: hello@ldaniel.eu
+password: >
+  $2a$10$XHjyTBNb7HewG8O4FY//Q.Tfb.au4C20DJKzVDdSNtHcCvnJwvlTe
+language: en
+role: admin
