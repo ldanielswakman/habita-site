ade85a7280f8754bac54d7c124d20451f6a2b15c
Daniel Swakman <dswakman@oefenweb.nl>
2016-05-02 00:02:28 +0300

    tiny fixes

--- site/snippets/updates_tags.php
+++ site/snippets/updates_tags.php
@@ -4,10 +4,11 @@
   $updates_tags_ordered = array_count_values($updates_tags);
   arsort($updates_tags_ordered);
   ?>
+  <? if (param('tag')) :?>
+    <span class="tag"><?= html(param('tag')) ?><a href="<?= $site->language()->url() ?>" class="u-ml5"><i class="ion ion-close-round"></i></a></span>
+  <? endif ?>
   <? foreach($updates_tags_ordered as $tag => $amount): ?>
-    <? if (param('tag') == $tag) :?>
-      <span class="tag"><?= html($tag) ?><a href="<?= $site->language()->url() ?>" class="u-ml5"><i class="ion ion-close-round"></i></a></span>
-    <? else :?>
+    <? if (param('tag') != $tag) :?>
       <a href="<?= $site->language()->url() . '/tag:' . $tag ?>" class="tag<?= e(!$active, ' tag--inactive') ?>" data-amount="<?= $amount ?>"><?= html($tag) ?></a>
     <? endif ?>
   <? endforeach ?>
