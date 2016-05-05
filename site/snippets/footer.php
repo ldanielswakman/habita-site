b53f25e668a0c4b18093293c0af769499056531a
Daniel Swakman <dswakman@oefenweb.nl>
2016-05-05 18:29:07 +0300

    signature

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,8 +1,16 @@
   <footer class="footer u-pl80 u-pb50">
 
-    <div class="copyright">
+    <div class="u-mb20">
       <?php echo $site->copyright()->kirbytext() ?>
     </div>
+
+    <div class="u-aligncenter u-mr50">
+
+      <hr class="u-mv20" />
+
+      <p><small>Designed and built by <a href="http://www.ldaniel.eu/" target="_blank">ldaniel.eu</a>.</small></p>
+    </div>
+
 
   </footer>
 
