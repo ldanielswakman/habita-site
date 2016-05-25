99b2efaa981eaf8159a5556a40691c9a2c192083
Daniel Swakman <dswakman@oefenweb.nl>
2016-05-25 21:24:25 +0300

    added generated sitemap

---
+++ site/blueprints/xmlsitemap.php
@@ -0,0 +1,12 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: Sitemap
+pages: false
+files: false
+deletable: false
+hide: true
+fields:
+  info:
+    label: Info
+    type: info
+    text: Do not delete this page; there is no content to configure here but it needs exist for automatically generating a sitemap for SEO.
