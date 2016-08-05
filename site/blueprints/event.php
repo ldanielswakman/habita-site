d7ce4e27b5516aaa2e63c8015a0b3ab000e009a6
L Daniel Swakman <d.swakman@gmail.com>
2016-08-05 16:13:11 +0200

    event date and time

--- site/blueprints/event.php
+++ site/blueprints/event.php
@@ -12,6 +12,21 @@ fields:
   date:
     label: Event date
     type: date
+    width: 1/3
+  start_time:
+    label: Start time
+    type: time
+    width: 1/3
+    interval: 30
+  end_time:
+    label: End time
+    type: time
+    width: 1/3
+    interval: 30
+  info:
+    type: info
+    text: >
+      <span style="color: #999;">Leave the start and end time empty to hide them in the actual page</span>
   description:
     label: Description
     type: markdown
