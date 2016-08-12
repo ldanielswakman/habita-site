49eef819f426b08f8d9e411f2d5a992d7abed078
L Daniel Swakman <d.swakman@gmail.com>
2016-08-12 03:15:43 +0300

    removed nofollow from header

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,14 +1,11 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  <meta name="author" content="L Daniel Swakman, ldaniel.eu" />
 
   <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
   <meta name="description" content="<?php echo $site->description()->html() ?>">
   <meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
-
-  <!-- This value is to prevent Google Search indexing before the site is live -->
-  <!-- NB. REMOVE THIS TAG when switching to full site!!! -->
-  <meta name="robots" content="noindex,nofollow"/>
 
   <!-- Social share parameters -->
   <meta property="og:image" content="<?= url('assets/images/anemptyspace.jpg') ?>" />
