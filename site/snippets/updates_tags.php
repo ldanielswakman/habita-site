44f62fe761b44c57b4249ec95e5f3fc30549392c
L Daniel Swakman <d.swakman@gmail.com>
2016-08-17 20:07:56 +0300

    blog api fix

--- site/snippets/updates_tags.php
+++ site/snippets/updates_tags.php
@@ -8,6 +8,7 @@
     <span class="tag"><?= html(param('tag')) ?><a href="<?= $site->language()->url() ?>" class="u-ml5"><i class="ion ion-close-round"></i></a></span>
   <? endif ?>
   <? foreach($updates_tags_ordered as $tag => $amount): ?>
+    <? $active = (isset($active)) ? $active : ''; ?>
     <? if (param('tag') != $tag) :?>
       <a href="<?= $site->language()->url() . '/tag:' . $tag ?>" class="tag<?= e(!$active, ' tag--inactive') ?>" data-amount="<?= $amount ?>"><?= html($tag) ?></a>
     <? endif ?>
