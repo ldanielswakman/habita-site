14ecfdaa3edc5c923fa6757882e16d39ada2d746
L Daniel Swakman <d.swakman@gmail.com>
2016-10-16 01:21:15 +0300

    tweaks

--- site/controllers/home.php
+++ site/controllers/home.php
@@ -1,7 +1,7 @@
 <?
 return function($site, $pages, $page) {
 
-  require_once kirby()->roots()->controllers() . '/shared/contact_form_obj.php';
+  require_once(kirby()->roots()->controllers() . '/shared/contact_form_obj.php');
 
   return compact('contact_form_obj');
 
--- site/templates/home.php
+++ site/templates/home.php
@@ -8,6 +8,8 @@
 
     <? snippet('nav') ?>
 
+    <? snippet('contactform') ?>
+    
     <? snippet('language-switcher') ?>
 
     <a href="#" onclick="return toggleMenu();" class="nav-logo nav-logo-animated">
