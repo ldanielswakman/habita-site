338bef3cf766b387f20310cf1b9d926126804c17
L Daniel Swakman <d.swakman@gmail.com>
2016-10-19 17:16:37 +0300

    test slack webhook

--- site/controllers/shared/contact_form_obj.php
+++ site/controllers/shared/contact_form_obj.php
@@ -13,6 +13,21 @@ $contact_form_obj = uniform('contact-form', [
         [
             '_action' => 'log',
             'file' => './email.log'
+        ],
+        [
+            '_action' => 'webhook',
+            'url' => 'https://hooks.slack.com/services/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',
+            'json' => true,
+            'params' => array(
+                'method' => 'post',
+                'payload' => array(
+                    'channel' => '#promotion',
+                    'username' => 'Contact Form (website)',
+                    'text' => 'This is posted to #promotion and comes from a bot named webhookbot.',
+                    'icon_emoji' => ':habita:'
+              )
+           )
+
         ]
     ]
 ]);
