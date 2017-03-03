780e6bddf17ba539e3a5cb0bc5c0b64803d9bf7b
L Daniel Swakman <d.swakman@gmail.com>
2017-03-03 00:59:20 -0300

    updated newsletter signup form

--- site/snippets/newsletter_signup.php
+++ site/snippets/newsletter_signup.php
@@ -2,9 +2,10 @@
 <? $showtelephone = (isset($telephone)) ? $telephone : false; ?>
 <? $light = (isset($theme) && $theme == 'light') ? ' field-light' : ''; ?>
 
+
 <!-- Begin MailChimp Signup Form -->
 <div id="mc_embed_signup" class="u-aligncenter">
-  <form action="//habita.us13.list-manage.com/subscribe/post?u=f12afb9c7fac0362abb729571&amp;id=8b50cfe5d5" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+  <form action="//habita.us13.list-manage.com/subscribe/post?u=f12afb9c7fac0362abb729571&amp;id=c19329a214" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 
     <div id="mc_embed_signup_scroll" class="u-inlineblock">
 
@@ -14,7 +15,7 @@
       </div>
 
       <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-      <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_f12afb9c7fac0362abb729571_8b50cfe5d5" tabindex="-1" value=""></div>
+      <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_f12afb9c7fac0362abb729571_c19329a214" tabindex="-1" value=""></div>
 
       <div class="mc-field-group u-floatleft u-mr20">
         <input type="email" value="" name="EMAIL" class="field <?= $light ?> required email" id="mce-EMAIL" placeholder="<?= l::get('email_address') ?>" style="width: 160px;">
