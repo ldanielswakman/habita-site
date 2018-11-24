c012945cb21ed08c1ebc96ba7a8926c7cbd1892d
ldanielswakman <d.swakman@gmail.com>
2018-11-24 14:50:24 +0100

    removed superfluous contact form email

--- site/controllers/shared/contact_form_obj.php
+++ site/controllers/shared/contact_form_obj.php
@@ -25,13 +25,13 @@ $contact_form_obj = uniform('contact-form', [
         '_from' => 'email'
     ],
     'actions' => [
-        [
-            '_action' => 'email',
-            'to'      => 'd.swakman@gmail.com',
-            'sender'  => 'info@habita.com.tr',
-            'subject' => '[' . $site->title()->html() . '] New Contact request - ' . $name,
-            'snippet' => 'email-contactform'
-        ],
+        // [
+        //     '_action' => 'email',
+        //     'to'      => 'd.swakman@gmail.com',
+        //     'sender'  => 'info@habita.com.tr',
+        //     'subject' => '[' . $site->title()->html() . '] New Contact request - ' . $name,
+        //     'snippet' => 'email-contactform'
+        // ],
         [
             '_action' => 'email',
             'to'      => 'info@habita.com.tr',
